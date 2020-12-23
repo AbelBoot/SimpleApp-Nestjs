9ecac61b39faf460cfd1956e8c4c1d22984e7433
Abel <abellocati@homail.com>
2020-12-23 23:18:54 +0000

    front end all set

---
+++ front/src/style/Containers.tsx
@@ -0,0 +1,25 @@
+import styled  from "styled-components"
+
+export const Container = styled.div`
+  padding: 1.5em 1.5em 1.5em 1.5em;
+  display: flex;
+  flex-direction: column;
+  background-color: #1D1F22;
+`
+
+export const ExistingNotesContainer = styled.div`
+`
+
+export const NoteContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+export const SingleElementContainer = styled.div`
+  padding-top: 1.5em;
+`
+
+export const NoteCreatorContainer = styled.div`
+  padding-top: 1.5em;
+  align-self: center;
+`
---
+++ front/src/style/SmallStyledComponents.tsx
@@ -0,0 +1,21 @@
+import styled  from "styled-components"
+
+export const H3 = styled.h3`
+    color: white;
+    text-align: center;
+`;
+
+export const Button = styled.button`
+    background: transparent;
+    border-radius: 3px;
+    border: 2px solid palevioletred;
+    color: palevioletred;
+    margin: 0 1em;
+    padding: 0.4em 1em;
+`;
+
+export const Input = styled.input`
+    border-radius: 5px 10px 5px;
+    padding: 6px 10px 6px 10px;
+    background-color: #D3D3D3;
+`;
